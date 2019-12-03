f11e5a08c20634cbd63bbaed4bee515353770641
Alexandra Osipova <AlexOsipova13@gmail.com>
2019-12-03 17:31:19 +0300

    Update build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,9 +14,11 @@ repositories {
 dependencies {
     val spekVersion = "2.0.8"
 
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.0")
     implementation(kotlin("stdlib-jdk8"))
 
     testImplementation("org.spekframework.spek2:spek-dsl-jvm:$spekVersion")
+    testImplementation(kotlin("test"))
     testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:$spekVersion")
     testRuntimeOnly(kotlin("reflect"))
 }
