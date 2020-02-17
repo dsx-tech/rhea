f33fab1089969d39b7d247257dd6ee28ac7aa55f
Alexandra Osipova <AlexOsipova13@gmail.com>
2020-02-17 13:46:52 +0300

    Add realizations of PropertyType for primitive types

--- src/main/kotlin/uk/dsx/reactiveconfig/ConfigManagerBase.kt
+++ src/main/kotlin/uk/dsx/reactiveconfig/ConfigManagerBase.kt
@@ -53,9 +53,44 @@ abstract class ConfigManagerBase {
 
     class StringType : PropertyType<String>() {
         override var initial: String = ""
-
         override fun parse(value: String): String? {
             return value
         }
     }
+
+    class IntType : PropertyType<Int>() {
+        override var initial: Int = 0
+        override fun parse(value: String): Int? {
+            return value.toIntOrNull()
+        }
+    }
+
+    class LongType : PropertyType<Long>() {
+        override var initial: Long = 0
+        override fun parse(value: String): Long? {
+            return value.toLongOrNull()
+        }
+    }
+
+    class FloatType : PropertyType<Float>() {
+        override var initial: Float = 0.0F
+        override fun parse(value: String): Float? {
+            return value.toFloatOrNull()
+        }
+    }
+
+    class DoubleType : PropertyType<Double>() {
+        override var initial: Double = 0.0
+        override fun parse(value: String): Double? {
+            return value.toDoubleOrNull()
+        }
+    }
+
+    class BooleanType : PropertyType<Boolean>() {
+        override var initial: Boolean = false
+        override fun parse(value: String): Boolean? {
+            return value.toBoolean()
+        }
+    }
+
 }
