f7767b845e523ec4425e51b49a734273d7468a7d
Alexandra Osipova <AlexOsipova13@gmail.com>
2020-02-20 12:24:23 +0300

    Refactor Reloadable

--- src/main/kotlin/uk/dsx/reactiveconfig/Reloadable.kt
+++ src/main/kotlin/uk/dsx/reactiveconfig/Reloadable.kt
@@ -3,20 +3,22 @@ package uk.dsx.reactiveconfig
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
-class Reloadable<T>(@Volatile private var value: T, private var observable: Flow<T>) {
+class Reloadable<T>(@Volatile private var value: T, private var flowOfChanges: Flow<T>) {
     init {
         ConfigManagerBase.configScope.launch {
-            observable.collect {
-                value = it
+            flowOfChanges.collect { newValue: T ->
+                value = newValue
             }
         }
     }
 
     fun get() = value
 
-    suspend fun onChange(function: (T) -> Unit) {
-        observable.collect() {
-            function(it)
+    fun onChange(function: (T) -> Unit) {
+        ConfigManagerBase.configScope.launch {
+            flowOfChanges.collect { newValue: T ->
+                function(newValue)
+            }
         }
     }
 }
