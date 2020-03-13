07000db55d3ec404081572fda4411a0a8c81b725
vologin-dmitry <etapochradlyavas@gmail.com>
2020-03-13 11:38:42 +0300

    Merge branch 'dev' of C:\Users\Дмитрий\Desktop\Java-Reactive-Configuration with conflicts.

--- src/main/kotlin/uk/dsx/reactiveconfig/ReactiveConfig.kt
+++ src/main/kotlin/uk/dsx/reactiveconfig/ReactiveConfig.kt
@@ -1,46 +1,27 @@
 package uk.dsx.reactiveconfig
 
-import uk.dsx.reactiveconfig.interfaces.ConfigSource
-import kotlin.reflect.KProperty
 
-class ReactiveConfig(block: ReactiveConfig.() -> Unit) {
-    var manager: ConfigManager = ConfigManager()
-    var base: PropertyTypeBase = PropertyTypeBase(manager)
-
+open class ReactiveConfig(block: ReactiveConfig.() -> Unit) {
     init {
         apply(block)
     }
 
-    infix fun <T : Any> String.of(type: PropertyTypeBase.PropertyType<T>) {
-        ReloadableFactory.createReloadable(this, type, manager)
+    val manager = ConfigManager
+
+    infix fun <T : Any> String.of(type: PropertyType<T>) {
+        ReloadableFactory.createReloadable(this, type)
     }
 
-    fun <T : Any> reloadable(key: String, type: PropertyTypeBase.PropertyType<T>): Reloadable<T> {
-        return ReloadableFactory.createReloadable(key, type, manager)
-    }
-
-    fun addConfigSource(source: ConfigSource) {
-        manager.addSource(source)
-    }
-}
-
-class PropertyTypeBase(val configManager: ConfigManager) {
-
-    inner class PropertyType<T : Any>(val initial: T, val parse: (String) -> T?) {
-        operator fun getValue(thisRef: Any?, property: KProperty<*>): Reloadable<T> {
-            return ReloadableFactory.createReloadable(property.name, this, configManager)
+    companion object {
+        fun <T : Any> reloadable(key: String, type: PropertyType<T>): Reloadable<T> {
+            return ReloadableFactory.createReloadable(key, type)
         }
     }
 
-    val stringType = PropertyType("", { it })
-
-    val intType = PropertyType(0, String::toIntOrNull)
-
-    val longType = PropertyType(0, String::toLongOrNull)
-
-    val floatType = PropertyType(0.0F, String::toFloatOrNull)
-
-    val doubleType = PropertyType(0.0, String::toDoubleOrNull)
-
-    val booleanType = PropertyType(false, String::toBoolean)
+    class PropertyGroup : ReactiveConfig()
+    {
+        
+    }
+    operator fun get(key : String) = manager.properties[key]
 }
+ 
