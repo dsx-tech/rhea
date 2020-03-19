c6f7c56f559597add72b0f4edd2e18a37d512024
Alexandra Osipova <alexosipova13@gmail.com>
2020-03-19 21:54:49 +0300

    Add support for nullable PropertyTypes

--- src/main/kotlin/uk/dsx/reactiveconfig/PropertyTypes.kt
+++ src/main/kotlin/uk/dsx/reactiveconfig/PropertyTypes.kt
@@ -12,53 +12,72 @@ data class StringNode(val value: String) : Node()
 data class ArrayNode(val value: MutableList<Node?>) : Node()
 data class BooleanNode(val value: Boolean) : Node()
 
-class PropertyTypeBase(val map: ConcurrentHashMap<String, Reloadable<*>>, val flowOfChanges: Flow<RawProperty>, val scope: CoroutineScope) {
+sealed class ParseResult<T>
+class Success<T>(val value: T) : ParseResult<T>()
+class Failure<T> : ParseResult<T>()
 
-    inner class PropertyType<T : Any>(val initial: T, val parse: (Node?) -> T?) {
+class PropertyTypeBase(
+    val map: ConcurrentHashMap<String, Reloadable<*>>,
+    val flowOfChanges: Flow<RawProperty>,
+    val scope: CoroutineScope
+) {
+
+    inner class PropertyType<T>(var initial: T, var parse: (Node?) -> ParseResult<T?>) {
         operator fun getValue(thisRef: Any?, property: KProperty<*>): Reloadable<T> {
             return ReloadableFactory.createReloadable(property.name, this, map, flowOfChanges, scope)
         }
     }
 
-    val stringType = PropertyType("", { node: Node? ->
+    fun <T> PropertyType<T>.nullable(): PropertyType<T?> {
+        return PropertyType(this.initial, { node: Node? ->
+            this.parse(node).let { result: ParseResult<T?> ->
+                when (result) {
+                    is Success -> result
+                    is Failure -> Success(null)
+                }
+            }
+        })
+    }
+
+    val stringType: PropertyType<String> = PropertyType("", { node: Node? ->
         when (node) {
-            is StringNode -> node.value
-            else -> return@PropertyType null
+            is StringNode -> Success(node.value)
+            else -> Failure()
         }
     })
 
-    val intType = PropertyType(0, { node: Node? ->
+    val intType: PropertyType<Int> = PropertyType(0, { node: Node? ->
         when (node) {
-            is NumericNode -> node.value.toInt()
-            else -> return@PropertyType null
+            is NumericNode -> Success(node.value.toInt())
+            else -> Failure()
         }
     })
 
-    val longType = PropertyType(0L, { node: Node? ->
+    val longType: PropertyType<Long> = PropertyType(0L, { node: Node? ->
         when (node) {
-            is NumericNode -> node.value.toLong()
-            else -> return@PropertyType null
+            is NumericNode -> Success(node.value.toLong())
+            else -> Failure()
         }
     })
 
-    val floatType = PropertyType(0.0F, { node: Node? ->
+    val floatType: PropertyType<Float> = PropertyType(0.0F, { node: Node? ->
         when (node) {
-            is NumericNode -> node.value.toFloat()
-            else -> return@PropertyType null
+            is NumericNode -> Success(node.value.toFloat())
+            else -> Failure()
         }
     })
 
-    val doubleType = PropertyType(0.0, { node: Node? ->
+    val doubleType: PropertyType<Double> = PropertyType(0.0, { node: Node? ->
         when (node) {
-            is NumericNode -> node.value.toDouble()
-            else -> return@PropertyType null
+            is NumericNode -> Success(node.value.toDouble())
+            else -> Failure()
         }
     })
 
-    val booleanType = PropertyType(false, { node: Node? ->
+    val booleanType: PropertyType<Boolean> = PropertyType(false, { node: Node? ->
         when (node) {
-            is BooleanNode -> node.value
-            else -> return@PropertyType null
+            is BooleanNode -> Success(node.value)
+            else -> Failure()
         }
     })
 }
--- src/main/kotlin/uk/dsx/reactiveconfig/ReloadableFactory.kt
+++ src/main/kotlin/uk/dsx/reactiveconfig/ReloadableFactory.kt
@@ -3,14 +3,14 @@ package uk.dsx.reactiveconfig
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.flow.mapNotNull
+import kotlinx.coroutines.flow.map
 import mu.KotlinLogging
 import java.util.concurrent.ConcurrentHashMap
 
 private val logger = KotlinLogging.logger {}
 
 object ReloadableFactory {
-    fun <T : Any> createReloadable(
+    fun <T> createReloadable(
         key: String,
         type: PropertyTypeBase.PropertyType<T>,
         map: ConcurrentHashMap<String, Reloadable<*>>,
@@ -20,10 +20,15 @@ object ReloadableFactory {
         return map[key] as Reloadable<T>? ?: Reloadable(type.initial,
             flowOfChanges.filter { rawProperty: RawProperty ->
                 rawProperty.key == key
-            }.mapNotNull { rawProperty: RawProperty ->
-                type.parse(rawProperty.value).also { result: T? ->
-                    result ?: logger.error("Wrong type of property: $key")
+            }.map { rawProperty: RawProperty ->
+                type.parse(rawProperty.value).let {
+                    when (it) {
+                        is Success -> it.value
+                        is Failure -> logger.error("Wrong type of property: $key")
+                    }
                 }
+            }.map {
+                it as T
             }, scope
         ).also { map[key] = it }
     }
